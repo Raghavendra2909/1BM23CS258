302eb84e97d7d47217edef9f56981868d974b4c0
Raghavendra2909 <raghavendraashok.cs23@bmsce.ac.in>
2024-10-23 10:53:18 +0530

    Update majorityelementleetcode.c

--- majorityelementleetcode.c
+++ majorityelementleetcode.c
@@ -1,27 +1,15 @@
-int compare(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b);
-}
-
-int majorityElement(int* nums, int numsSize) {
-    // Sort the array
-    qsort(nums, numsSize, sizeof(int), compare);
-    
-    int count = 1;
-    int max_count = 1;
-    int result = nums[0];
-
-    for (int i = 1; i < numsSize; i++) {
-        if (nums[i] == nums[i - 1]) {
-            count++;
+int majorityElement(int* nums, int numsSize){
+    int sol = nums[0],
+    cnt = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (cnt == 0) {
+            sol = nums[i];
+        }
+        if (nums[i] == sol) {
+            cnt++;
         } else {
-            count = 1;
-        }
-
-        if (count > max_count) {
-            max_count = count;
-            result = nums[i];
-        }
-    }
-
-    return result;
+            cnt--;
+        } 
+    }        
+    return sol;
 }
